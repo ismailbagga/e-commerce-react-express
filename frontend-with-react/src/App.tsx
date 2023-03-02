e229d22cbddf466af7d7c63ca5684acf353d53a5
Ismail <ismailbak7@gmail.com>
2023-03-02 17:54:10 +0100

    Add React folder with docker config & tailwind

---
+++ frontend-with-react/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="bg-black text-pink-300">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
