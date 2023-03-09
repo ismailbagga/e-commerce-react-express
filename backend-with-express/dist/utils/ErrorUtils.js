f2ef910963a5aae66328cd51eb26cb45574f71e8
Ismail Ba <ismailbak7@gmail.com>
2023-03-09 15:58:29 +0100

    Remove Dist Folder

--- backend-with-express/dist/utils/ErrorUtils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseZodError = void 0;
-const parseZodError = (zodError) => {
-    return " is required ";
-};
-exports.parseZodError = parseZodError;
