26ebfa70aaaafb797b9710aa8ecb761d7b947588
Ismail Ba <ismailbak7@gmail.com>
2023-03-22 11:35:18 +0000

    Add Multiple Carousels To Home

--- packages/frontend-with-react/src/pages/Home.tsx
+++ packages/frontend-with-react/src/pages/Home.tsx
@@ -5,6 +5,14 @@ const HomePage = () => {
   return (
     <h1>
       <ProductsCarousels listingCategory="latest" title="Latest Products" />
+      <ProductsCarousels
+        listingCategory="top-rated"
+        title="Top Rated Products"
+      />
+      <ProductsCarousels
+        listingCategory="top-selling"
+        title="Top Selling Products"
+      />
     </h1>
   )
 }
