8d55670b9c4946c60c185fc11115f61ea3d5f84b
Ismail Ba <ismailbak7@gmail.com>
2023-03-27 12:24:09 +0000

    Add Search page Filter Side Nav

--- packages/frontend-with-react/src/pages/FilterPage.tsx
+++ packages/frontend-with-react/src/pages/FilterPage.tsx
@@ -7,7 +7,10 @@ const RatingValue: FC<{ minRating: 1 | 2 | 3 | 4 }> = ({ minRating }) => {
     console.log('min rating ', minRating)
   }
   return (
-    <div className="flex space-x-1" onClick={clickHandler}>
+    <div
+      className="mb-2 flex w-fit space-x-1 border-2 border-transparent hover:border-red-600"
+      onClick={clickHandler}
+    >
       {STARS.map((val, index) => {
         console.log(val)
 
@@ -23,43 +26,52 @@ const RatingValue: FC<{ minRating: 1 | 2 | 3 | 4 }> = ({ minRating }) => {
     </div>
   )
 }
+
+const Input: FC<{ placeholder: string }> = ({ placeholder }) => {
+  return (
+    <input
+      type="number"
+      className=" w-16 rounded border-2 border-gray-600 px-1 py-1"
+      min="0"
+      placeholder={placeholder}
+    />
+  )
+}
+
 const PriceForm = () => {
   return (
     <form className="flex h-8 space-x-2 ">
-      <input
-        type="number"
-        className=" w-14 rounded border-2 border-gray-600 px-1 py-1"
-        min="0"
-        placeholder="Min"
-      />
-      <input
-        type="number"
-        className="w-14 rounded border-2 border-gray-600 px-1 py-1"
-        min="0"
-        placeholder="Max"
-      />
+      <Input placeholder="Min" />
+      <Input placeholder="Max" />
+
       <button className=" rounded bg-slate-800 px-5 text-white">Go</button>
     </form>
   )
 }
+
+const SearchHeader: FC<{ text: string }> = ({ text }) => {
+  return <h1 className="mb-3 text-xl font-bold">{text}</h1>
+}
+
 const FilterPage = () => {
   const stars = [4, 3, 2, 1] as const
   return (
-    <section>
-      <header className="ml-10 mt-10">
-        <h1>Filter</h1>
-        <div>
-          <h1>Rating</h1>
+    <section className="mx-5 mt-10 flex">
+      <header className="  mr-5 border-r-2 border-gray-600 pr-4">
+        <h1 className="text-4xl font-bold tracking-wide">Filter</h1>
+        <div className="mt-5">
+          <SearchHeader text="Rating" />
+          {/* <h1 className="mb-3 text-xl font-bold">Rating</h1> */}
           {stars.map((star) => (
             <RatingValue key={star} minRating={star} />
           ))}
         </div>
-        <div className="">
-          <h1>Price</h1>
+        <div className="mt-5">
+          <SearchHeader text="Price" />
           <PriceForm />
         </div>
       </header>
-      <main></main>
+      <main className="h-56 grow bg-black"></main>
     </section>
   )
 }
